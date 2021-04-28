be36cdde8666090ee489bfae29011e97f2c53ecb
Simon Hensel <simon.hensel@protonmail.com>
2021-04-28 13:20:43 +0200

    streamlined table names and added comments

--- database/dump.sql
+++ database/dump.sql
@@ -9,7 +9,7 @@ USE teachingbee;
 DROP TABLE IF EXISTS interests;
 CREATE TABLE interests (
   id int AUTO_INCREMENT,
-  name varchar(255) NOT NULL,
+  iname varchar(255) NOT NULL,  -- iname = interest name
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -18,16 +18,15 @@ CREATE TABLE profile (
   id int AUTO_INCREMENT,
   stamp timestamp NOT NULL,
   course varchar(255) NOT NULL,
-  studytype varchar(255) NOT NULL,
-  extroverted int NOT NULL,
-  frequency int NOT NULL,
-  online int NOT NULL,
-  /*on_off varchar(3) NOT NULL,*/
+  studytype varchar(255) NOT NULL,  -- int or varchar?
+  extroverted int NOT NULL,         -- value between 1 and 5
+  frequency int NOT NULL,           -- value between 1 and 5
+  online int NOT NULL,              -- online is either 1 or 0 (0 = offline)
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS interests_in_profile;
-CREATE TABLE interests_in_profile (
+DROP TABLE IF EXISTS r_interests_profile;
+CREATE TABLE r_interests_profile (
   id int AUTO_INCREMENT,
   profileID int NOT NULL,
   interestID int NOT NULL,
@@ -40,11 +39,11 @@ DROP TABLE IF EXISTS person;
 CREATE TABLE person (
   id int NOT NULL AUTO_INCREMENT,
   stamp timestamp NOT NULL,
-  fname varchar(100) NOT NULL,
-  lname varchar(100) NOT NULL,
+  fname varchar(100) NOT NULL,    -- fname = first name
+  lname varchar(100) NOT NULL,    -- lname = last name
   birthdate date NOT NULL,
   semester int NOT NULL,
-  gender varchar(100) NOT NULL,
+  gender varchar(100) NOT NULL,   -- or int?
   profileID int,
   PRIMARY KEY (id),
   FOREIGN KEY (profileID) REFERENCES profile(id)
@@ -54,14 +53,14 @@ DROP TABLE IF EXISTS studygroup;
 CREATE TABLE studygroup (
   id int NOT NULL AUTO_INCREMENT,
   stamp timestamp NOT NULL,
-  gname varchar(255),
-  head int NOT NULL,
+  gname varchar(255),         -- gname = group name
+  head int NOT NULL,          -- head = group admin
   PRIMARY KEY (id),
   FOREIGN KEY (head) REFERENCES person(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS person_in_group;
-CREATE TABLE person_in_group (
+DROP TABLE IF EXISTS r_person_group;
+CREATE TABLE r_person_group (
   id int NOT NULL AUTO_INCREMENT,
   groupID int NOT NULL,
   personID int NOT NULL,
@@ -79,6 +78,7 @@ CREATE TABLE chat (
   PRIMARY KEY (id),
   FOREIGN KEY (groupID) REFERENCES studygroup(id)
   -- FK for person ???
+  -- how to differentiate between group and person?
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS message;
@@ -93,8 +93,8 @@ CREATE TABLE message (
   FOREIGN KEY (chatID) REFERENCES chat(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS person_in_chat;
-CREATE TABLE person_in_chat (
+DROP TABLE IF EXISTS r_person_chat;
+CREATE TABLE r_person_chat (
   id int NOT NULL AUTO_INCREMENT,
   personID int NOT NULL,
   PRIMARY KEY (id),
@@ -114,13 +114,13 @@ CREATE TABLE request (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- insert values into tables
-INSERT INTO interests (name) VALUES ('Sport');
-INSERT INTO interests (name) VALUES ('Mathe');
-INSERT INTO interests (name) VALUES ('Programmieren');
+INSERT INTO interests (iname) VALUES ('Sport');
+INSERT INTO interests (iname) VALUES ('Mathe');
+INSERT INTO interests (iname) VALUES ('Programmieren');
 
 INSERT INTO profile (stamp, course, studytype, extroverted, frequency, online) VALUES ('2000-01-01 10:00:00', 'WI', 'audiovisuell', 3, 3, 1);
 
-INSERT INTO interests_in_profile (profileID, interestID) VALUES (1, 1);
-INSERT INTO interests_in_profile (profileID, interestID) VALUES (1, 2);
+INSERT INTO r_interests_profile (profileID, interestID) VALUES (1, 1);
+INSERT INTO r_interests_profile (profileID, interestID) VALUES (1, 2);
 
 INSERT INTO person (stamp, fname, lname, birthdate, semester, gender, profileID) VALUES ('2000-01-01 10:00:00', 'Paula', 'Pudding', '2000-01-01', 3, 'female', 1);
