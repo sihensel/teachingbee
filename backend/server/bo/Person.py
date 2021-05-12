80097f54fbc84cccf20de5293d5877630fd3c212
Simon Hensel <simon.hensel@protonmail.com>
2021-05-12 13:23:49 +0200

    init commit

---
+++ backend/server/bo/Person.py
@@ -0,0 +1,45 @@
+import server.bo.BusinessObject as bo
+
+class Person(bo.BusinessObject):
+    def __init__(self):
+        super().__init__()
+        self._fname = ''
+        self._lname = ''
+        self._birthdate = ''
+        self._age = 0
+        self._semester = 0
+        self._gender = ''
+
+    def set_fname(self, fname):
+        self._fname = fname
+
+    def get_fname(self):
+        return self._fname
+
+    def set_lname(self, lname):
+        self._lname = lname
+
+    def get_lname(self):
+        return self._lname
+    
+    def set_birthdate(self, birthdate):
+        self._birthdate = birthdate
+
+    def get_birthdate(self):
+        return self._birthdate
+
+    #def get_age(self):
+    #    self._age = datetime.now().year - self._birthdate.year
+    #   return self._age
+
+    def set_semester(self, semester):
+        self._semester = semester
+
+    def get_semester(self):
+        return self._semester
+
+    def set_gender(self, gender):
+        self._gender = gender
+
+    def get_gender(self):
+        return self._gender
---
+++ reactfrontend/src/ManagePerson.js
@@ -0,0 +1,112 @@
+import React from 'react';
+
+class ManagePerson extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        fname: '',
+        lname: '',
+        birthdate: '',
+        semester: '',
+        gender: ''
+      };
+        fetch('http://localhost:5000/person', {
+            method: 'get'})
+            .then(response => response.json())
+            .then(data => this.setState({ fname: data.fname, lname: data.lname, birthdate: data.birthdate, semester: data.semester, gender: data.gender}));
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h1>SignUp</h1>
+                <form>
+                    Vorname: <br/>
+                    <input type="text" value={this.state.fname} onChange={evt => this.updateFname(evt)}></input> <br/>
+                    Nachname: <br/>
+                    <input type="text" value={this.state.lname} onChange={evt => this.updateLname(evt)}></input><br/>
+                    Geburtsdatum:<br/>
+                    <input type="date" value={this.state.birthdate} onChange={evt => this.updateBirthdate(evt)}></input><br/>
+                    Semester: <br/>
+                    <input type="text" value={this.state.semester} onChange={evt => this.updateSemester(evt)}></input><br/>
+                    <label htmlFor="gender">Geschlecht:</label><br/>
+                        <select id="gender" value={this.state.gender} onChange={evt => this.updateGender(evt)}>
+                            <option value="">{this.state.gender}</option>
+                            <option value="f">Weiblich</option>
+                            <option value="m">Männlich</option>
+                            <option value="d">Diverse</option>
+                        </select><br/>
+                </form>
+                <button onClick={this.handleSubmit.bind(this)}>Absenden</button>
+            </div>
+      );
+    }
+  
+    handleSubmit(){
+      // Simple POST request with a JSON body using fetch
+      /*
+      const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                fname: this.state.fname.valueOf(),
+                lname: this.state.lname.valueOf(),
+                date: this.state.date,
+                semester: this.state.semester.valueOf(),
+                gender: this.state.gender.valueOf()
+                })
+         };
+        fetch('https://localhost:5000/api', requestOptions)
+            .then(response => response.json())
+            //.then(data => this.setState({ postId: data.id }));
+            //console.log(response.json())
+            */
+
+        fetch('http://127.0.0.1:5000/api', {
+            method: 'post',
+            headers: {'content-type': 'application/json'},
+            body: JSON.stringify({
+                "fname": this.state.fname,
+                "lname": this.state.lname,
+                "birthdate": this.state.birthdate,
+                "semester": this.state.semester,
+                "gender": this.state.gender
+                })
+            })
+        }
+        
+    
+
+    updateFname(evt) {
+        this.setState({
+          fname: evt.target.value
+        });
+      }
+
+    updateLname(evt) {
+        this.setState({
+            lname: evt.target.value
+        });
+        }
+
+    updateBirthdate(evt) {
+        this.setState({
+            birthdate: evt.target.value
+        });
+    }
+
+    updateSemester(evt) {
+        this.setState({
+            semester: evt.target.value
+        });
+    }
+
+    updateGender(evt) {
+        this.setState({
+            gender: evt.target.value
+        });
+      }
+  };
+
+export default ManagePerson;
