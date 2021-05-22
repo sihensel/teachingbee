7c2a71548cae24efd4cc52e714ef56acff69802e
Simon Hensel <simon.hensel@protonmail.com>
2021-05-22 16:53:49 +0200

    added selection for interests and modified all values to match database

--- reactfrontend/src/ManagePerson.js
+++ reactfrontend/src/ManagePerson.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Select from 'react-select';
+
 
 class ManagePerson extends React.Component {
     constructor(props) {
@@ -15,62 +17,99 @@ class ManagePerson extends React.Component {
             extroverted: '',
             frequency: '',
             online: '',
-            profileID: ''
+            profileID: '',
+            interests: [],          // Interessen aus der Datenbank
+            selectedinterests: [],  // in React selektierte Interessen
+            options: [],
+            selected: []
         };
-        fetch('http://localhost:5000/person/', {
+    }
+
+    componentDidMount() {
+        this.getOptions()
+    }
+    async getOptions() {
+        await fetch("http://localhost:5000/create_profile", {
+            method: "get",
+        })
+            .then((response) => response.json())
+            .then((json) => {
+                this.setState({ interests: json })
+            });
+        await fetch('http://localhost:5000/person/', {
             method: 'get'
         })
             .then(response => response.json())
             .then(data => this.setState({
                 id: data.id, fname: data.fname, lname: data.lname, birthdate: data.birthdate, semester: data.semester, gender: data.gender,
-                course: data.course, studytype: data.studytype, extroverted: data.extroverted, frequency: data.frequency, online: data.online, profileID: data.profileID
+                course: data.course, studytype: data.studytype, extroverted: data.extroverted, frequency: data.frequency, online: data.online, profileID: data.profileID, selectedinterests: data.interests
             }));
+
+        // Auswahlmöglichkeiten für die Interessen
+        const selectoptions = this.state.interests.map(item => ({
+            "value": item[0],   // ID
+            "label": item[1]    // Name
+        }))
+        this.setState({ options: selectoptions })
+
+        // Interessen aus der Datenbank
+        const sel = []
+        for (let i = 0; i < this.state.selectedinterests.length; i++) {
+            sel.push(this.state.selectedinterests[i] - 1)
+        }
+        this.setState({ selected: sel })
     }
 
     render() {
         /* Reihenfolge des Dropdown-Menüs mit den Geschlechtern festlegen
         das Geschlecht aus der Datenbank soll immer als erstes angezeigt werden */
-        const genderList = ['weiblich', 'männlich', 'divers'];  // vllt aus der Datenbank auslesen??
-
+        const genderList = ['weiblich', 'männlich', 'divers'];
         // das Geschlecht aus der Datenbank aus der Liste entfernen
         for (let i = 0; i < genderList.length; i++) {
             if (genderList[i] === this.state.gender) {
                 genderList.splice(i, 1);
-                i--;   // es muss wieder ein Index zurückgegangen werden, da ein Element gelöscht wurde
+                i--;
             }
         }
-        const courseList = ['WI', 'ID', 'IW', 'OM'];  // vllt aus der Datenbank auslesen??
 
+        const courseList = ['WI', 'ID', 'IW', 'OM'];
         // den Studiengang aus der Datenbank aus der Liste entfernen
         for (let i = 0; i < courseList.length; i++) {
             if (courseList[i] === this.state.course) {
                 courseList.splice(i, 1);
-                i--;   // es muss wieder ein Index zurückgegangen werden, da ein Element gelöscht wurde
+                i--;
             }
         }
 
-        const studytypeList = ['auditiv', 'kommunikativ', 'motorisch', 'visuell'];  // vllt aus der Datenbank auslesen??
-
-        // das Lerntyp aus der Datenbank aus der Liste entfernen
+        const studytypeList = ['auditiv', 'kommunikativ', 'motorisch', 'visuell'];
+        // den Lerntyp aus der Datenbank aus der Liste entfernen
         for (let i = 0; i < studytypeList.length; i++) {
             if (studytypeList[i] === this.state.studytype) {
                 studytypeList.splice(i, 1);
-                i--;   // es muss wieder ein Index zurückgegangen werden, da ein Element gelöscht wurde
+                i--;
             }
         }
 
-        const onlineList = ['offline', 'online', 'beides'];  // vllt aus der Datenbank auslesen??
-
-        // das Lerntyp aus der Datenbank aus der Liste entfernen
+        const onlineList = ['offline', 'online', 'beides'];
+        // den Lernort aus der Datenbank aus der Liste entfernen
         for (let i = 0; i < onlineList.length; i++) {
             if (onlineList[i] === this.state.online) {
                 onlineList.splice(i, 1);
-                i--;   // es muss wieder ein Index zurückgegangen werden, da ein Element gelöscht wurde
+                i--;
+            }
+        }
+
+        const freqList = [1, 2, 3];
+        // den Lernort aus der Datenbank aus der Liste entfernen
+        for (let i = 0; i < freqList.length; i++) {
+            if (freqList[i] === this.state.frequency) {
+                freqList.splice(i, 1);
+                i--;
             }
         }
 
         return (
-            <div>
+            < div >
                 <h1>Profil bearbeiten</h1>
                 <form>
                     Vorname: <br />
@@ -101,16 +140,30 @@ class ManagePerson extends React.Component {
                         <option value={studytypeList[1]}>{studytypeList[1]}</option>
                         <option value={studytypeList[2]}>{studytypeList[2]}</option>
                     </select><br />
-                    Extrovertiertheit: <br />
-                    <input type="text" value={this.state.extroverted} onChange={evt => this.updateExtroverted(evt)}></input> <br />
-                    Lernhäufigkeit: <br />
-                    <input type="text" value={this.state.frequency} onChange={evt => this.updateFrequency(evt)}></input> <br />
+                    <label htmlFor="extroverted">Wie extrovertiert bist du?:</label>
+                    <br />
+                    {/* noch was schlaues überlegen! */}
+                    <select id="extroverted" value={this.state.extroverted} onChange={(evt) => this.updateExtroverted(evt)}>
+                        <option value={this.state.extroverted}>{this.state.extroverted}</option>
+                        <option value="2">mäßig</option>
+                        <option value="3">sehr</option>
+                    </select>< br />
+                    <label htmlFor="frequency">Lernfrequenz:</label>
+                    <br />
+                    <select id="frequency" value={this.state.frequency} onChange={(evt) => this.updateFrequency(evt)}>
+                        <option value={this.state.frequency}>{this.state.frequency} mal die Woche</option>
+                        <option value={freqList[0]}>{freqList[0]} mal die Woche</option>
+                        <option value={freqList[1]}>{freqList[1]} mal oder mehr</option>
+                    </select><br />
                     Onlinelernen: <br />
                     <select id="online" value={this.state.online} onChange={evt => this.updateOnline(evt)}>
                         <option value={this.state.online}>{this.state.online}</option>
                         <option value={onlineList[0]}>{onlineList[0]}</option>
                         <option value={onlineList[1]}>{onlineList[1]}</option>
                     </select><br />
+                    <label htmlFor="interests">Interessen (bitte max. 2 auswählen):</label>
+                    <br />
+                    <Select name='interests' value={[this.state.options[this.state.selected[0]], this.state.options[this.state.selected[1]]]} isMulti options={this.state.options} onChange={(evt) => this.updateInterests(evt)} />
                 </form>
                 <button onClick={this.handleSubmit.bind(this)}>Absenden</button>
             </div>
@@ -118,24 +171,30 @@ class ManagePerson extends React.Component {
     }
 
     handleSubmit() {
-        fetch('http://127.0.0.1:5000/person/', {
-            method: 'post',
-            headers: { 'content-type': 'application/json' },
-            body: JSON.stringify({
-                "id": this.state.id,
-                "fname": this.state.fname,
-                "lname": this.state.lname,
-                "birthdate": this.state.birthdate,
-                "semester": this.state.semester,
-                "gender": this.state.gender,
-                "course": this.state.course,
-                "studytype": this.state.studytype,
-                "extroverted": this.state.extroverted,
-                "frequency": this.state.frequency,
-                "online": this.state.online,
-                "profileID": this.state.profileID
+        if (this.state.selectedinterests.length > 2) {
+            alert('bitte max. 2 interessen auswählen.');
+            // noch prüfen, ob alle Felder ausgefüllt wurden!
+        } else {
+            fetch('http://127.0.0.1:5000/person/', {
+                method: 'post',
+                headers: { 'content-type': 'application/json' },
+                body: JSON.stringify({
+                    "id": this.state.id,
+                    "fname": this.state.fname,
+                    "lname": this.state.lname,
+                    "birthdate": this.state.birthdate,
+                    "semester": this.state.semester,
+                    "gender": this.state.gender,
+                    "course": this.state.course,
+                    "studytype": this.state.studytype,
+                    "extroverted": this.state.extroverted,
+                    "frequency": this.state.frequency,
+                    "online": this.state.online,
+                    "profileID": this.state.profileID,
+                    "interests": this.state.selectedinterests,
+                })
             })
-        })
+        }
     }
 
     updateFname(evt) {
@@ -196,6 +255,9 @@ class ManagePerson extends React.Component {
         this.setState({
             online: evt.target.value
         });
+    }
+    updateInterests(evt) {
+        this.setState({ selectedinterests: evt })
     }
 };
 
