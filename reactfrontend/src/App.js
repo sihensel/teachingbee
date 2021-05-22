ded87f5390ba4820ff9a6180496507e7b259b144
Simon Hensel <simon.hensel@protonmail.com>
2021-05-22 10:19:43 +0200

    now displaying ManagePerson.js

--- reactfrontend/src/App.js
+++ reactfrontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Create_Profile from './Create_Profile';
 import Signup from './Signup.js';
+import ManagePerson from './ManagePerson.js';
 
 function App() {
   return (
-      <Create_Profile/>
+      //<Create_Profile/>
+      <ManagePerson/>
   );
 }
 
