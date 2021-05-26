199dbc96a831ac0a0d7ec54d9a5628c9f85678fd
Simon Hensel <simon.hensel@protonmail.com>
2021-05-26 22:50:18 +0200

    started integrating businesslogic

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 from flask_restx import Api, Resource, fields
 
+from server.BusinessLogic import BusinessLogic
 from server.db.InterestMapper import InterestMapper
 from server.bo.Person import Person
 from server.db.PersonMapper import PersonMapper
@@ -9,20 +10,85 @@ from server.bo.Profile import Profile
 from server.db.ProfileMapper import ProfileMapper
 
 app = Flask(__name__)
-CORS(app)
+#CORS(app)
+CORS(app, resources=r'/teachingbee/*')
 
-api = Api(app, version='0.1', title='Teachingbee', description='test')
+api = Api(app, version='0.1', title='Teachingbee', description='App um Lernpartner zu finden.')
 teachingbee = api.namespace('teachingbee', description='App zum finden von Lernpartnern')
+
+# Business Object
 bo = api.model('BusinessObject', {
     'id': fields.Integer(attribute='_id', description='ID'),
 })
+
+# Personenobjekt
 person = api.inherit('Person', bo, {
-    'fname': fields.String(attribute='_fname'),
-    'lname': fields.String(attribute='_lname'),
-    'birthdate': fields.String(attribute='_birthdate'),
-    'semester': fields.String(attribute='_semester'),
-    'gender': fields.String(attribute='_gender'),
+    'fname': fields.String(attribute='_fname', description='Vorname'),
+    'lname': fields.String(attribute='_lname', description='Nachname'),
+    'birthdate': fields.String(attribute='_birthdate', description='Geburtsdatum'),
+    'age': fields.Integer(attribute='_age', description='Alter'),               # vllt als String lassen?
+    'semester': fields.Integer(attribute='_semester', description='Semester'),  # vllt als String lassen?
+    'gender': fields.String(attribute='_gender', description='Geschlecht'),
+    'profileID': fields.Integer(attribute='_profileID', description='ID des Profils'),
 })
+
+# Profilobjekt
+profile = api.inherit('Profile', bo, {
+    'course': fields.String(attribute='_course', description='Studiengang'),
+    'studytype': fields.String(attribute='_studytype', description='Lerntyp'),
+    'extroverted': fields.String(attribute='_extroverted', description='Extrovertiertheit'),
+    'frequency': fields.String(attribute='_frequency', description='Lernhäufigkeit'),
+    'online': fields.String(attribute='_online', description='Online/Offline lernen'),
+})
+
+# POST: create
+# PUT: update
+# DELETE: delete
+
+@teachingbee.route('/interests')
+@teachingbee.response(500, 'Internal Server Error')
+class Interests(Resource):
+    def get(self):
+        bl = BusinessLogic()
+        interests = bl.get_all_interests()
+        return interests
+
+# eine einzelne Person auslesen
+@teachingbee.route('/person/<int:id>')
+@teachingbee.response(500, 'Internal Server Error')
+@teachingbee.param('id', 'ID der Person')
+class PersonOperations(Resource):
+    @teachingbee.marshal_with(person)
+    def get(self, id):
+        ''' Person aus der DB auslesen '''
+        bl = BusinessLogic()
+        pers = bl.get_person(id)
+        return pers
+
+    @teachingbee.marshal_with(person)
+    @teachingbee.expect(person, validate=True)
+    def put(self, id):
+        ''' Person updaten '''
+        pass
+
+
+@teachingbee.route('/profile/<int:id>')
+@teachingbee.response(500, 'Internal Server Error')
+@teachingbee.param('id', 'ID des Profils')
+class ProfileOperations(Resource):
+    @teachingbee.marshal_with(profile)
+    def get(self, id):
+        ''' Profil aus der DB auslesen '''
+        bl = BusinessLogic()
+        prof = bl.get_profile(id)
+        return prof
+
+    @teachingbee.marshal_with(profile)
+    @teachingbee.expect(profile, validate=True)
+    def put(self, id):
+        ''' Profil updaten '''
+        pass
+
 
 # werden von manage_person und create_profile benötigt
 int_mapper = InterestMapper()
