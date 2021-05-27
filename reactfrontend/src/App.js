ffac8bb9f0dba081fdbbbe007aa0edbaefef8932
Simon Hensel <simon.hensel@protonmail.com>
2021-05-27 23:15:34 +0200

    resolved merge conflict

--- reactfrontend/src/App.js
+++ reactfrontend/src/App.js
@@ -1,21 +1,12 @@
 import './App.css';
-<<<<<<< HEAD
 import Create_Profile from './Create_Profile';
 import Signup from './Signup.js';
-
-function App() {
-  return (
-      <Create_Profile/>
-=======
 import ManagePerson from './ManagePerson';
-//import Signup from './Signup.js';
 
 function App() {
   return (
       <ManagePerson/>
->>>>>>> main
   );
 }
-
 
 export default App;
