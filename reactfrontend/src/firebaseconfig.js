a86651bc44c01a05260b6a44c315ce571e989650
Simon Hensel <simon.hensel@protonmail.com>
2021-07-02 13:10:38 +0200

    final cleanup

--- reactfrontend/src/firebaseconfig.js
+++ reactfrontend/src/firebaseconfig.js
@@ -1,7 +1,12 @@
 // Hier die Daten von Firebase einfügen
 
 const firebaseConfig = {
-
+  apiKey: "AIzaSyBXOmY9X2h-19cDnLAjfbHbkmLpwlACNH4",
+  authDomain: "teachingbee-179c2.firebaseapp.com",
+  projectId: "teachingbee-179c2",
+  storageBucket: "teachingbee-179c2.appspot.com",
+  messagingSenderId: "588351100531",
+  appId: "1:588351100531:web:71373861bd4febbabc7c06"
 };
 
 export default firebaseConfig;
